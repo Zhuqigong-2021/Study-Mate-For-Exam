6adeddfcfebc8c5e6ef1c86ee272ee37329f9f32
Zhuqigong-2021 <1406846879@qq.com>
2024-02-05 22:29:00 -0500

    add frontresult client component

--- src/app/exam/[id]/result/page.tsx
+++ src/app/exam/[id]/result/page.tsx
@@ -2,6 +2,8 @@ import Pie from "@/components/Pie";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import { idProps } from "../page";
+import FrontResult from "@/components/FrontResult";
 
 const page = ({
   searchParams,
@@ -16,41 +18,42 @@ const page = ({
 }) => {
   let wrong = Number(searchParams.total) - Number(searchParams.correct);
   let id = searchParams.id;
-  console.log(id);
-  // console.log("this is the result" + searchParams.result);
-  return (
-    <div
-      className=" absolute left-[50%] top-[50%] flex w-full -translate-x-[50%] -translate-y-[50%] flex-col items-center justify-center "
-      suppressHydrationWarning={true}
-    >
-      <h2 className="mb-8 text-xl font-bold text-green-600">
-        {Number(searchParams.correct) / Number(searchParams.total) === 1 &&
-          "Congratulations you got full mark !!!"}
-      </h2>
+  // console.log(id);
 
-      <Pie right={Number(searchParams.correct)} wrong={wrong} />
-      <h2 className="my-4 text-xl font-bold">Your Test Result</h2>
-      <p>{id}</p>
-      <p className="text-red-500">
-        {/* {Math.round(
-          (Number(searchParams.correct) / Number(searchParams.total)) * 100,
-        ) + "%"} */}
-        {Number(searchParams.result) + "%"}
-      </p>
-      <Button asChild className="mt-4">
-        <Link
-          href={{
-            pathname: `/exam/${searchParams.id}/report`,
-            query: {
-              id: `${searchParams.id}`,
-              choiceId: `${searchParams.choiceId}`,
-            },
-          }}
-        >
-          get details report
-        </Link>
-      </Button>
-    </div>
+  return (
+    <FrontResult />
+    // <div
+    //   className=" absolute left-[50%] top-[50%] flex w-full -translate-x-[50%] -translate-y-[50%] flex-col items-center justify-center "
+    //   suppressHydrationWarning={true}
+    // >
+    //   <h2 className="mb-8 text-xl font-bold text-green-600">
+    //     {Number(searchParams.correct) / Number(searchParams.total) === 1 &&
+    //       "Congratulations you got full mark !!!"}
+    //   </h2>
+
+    //   <Pie right={Number(searchParams.correct)} wrong={wrong} />
+    //   <h2 className="my-4 text-xl font-bold">Your Test Result</h2>
+    //   <p>{id}</p>
+    //   <p className="text-red-500">
+    //     {/* {Math.round(
+    //       (Number(searchParams.correct) / Number(searchParams.total)) * 100,
+    //     ) + "%"} */}
+    //     {Number(searchParams.result) + "%"}
+    //   </p>
+    //   <Button asChild className="mt-4">
+    //     <Link
+    //       href={{
+    //         pathname: `/exam/${searchParams.id}/report`,
+    //         query: {
+    //           id: `${searchParams.id}`,
+    //           choiceId: `${searchParams.choiceId}`,
+    //         },
+    //       }}
+    //     >
+    //       get details report
+    //     </Link>
+    //   </Button>
+    // </div>
   );
 };
 
---
+++ src/components/FrontResult.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Pie from "@/components/Pie";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+
+const FrontResult = () => {
+  const searchParams = useSearchParams();
+
+  let wrong =
+    Number(searchParams.get("total")) - Number(searchParams.get("correct"));
+  let id = searchParams.get("id");
+  // console.log(id);
+
+  return (
+    <div
+      className=" absolute left-[50%] top-[50%] flex w-full -translate-x-[50%] -translate-y-[50%] flex-col items-center justify-center "
+      suppressHydrationWarning={true}
+    >
+      <h2 className="mb-8 text-xl font-bold text-green-600">
+        {Number(searchParams.get("correct")) /
+          Number(searchParams.get("total")) ===
+          1 && "Congratulations you got full mark !!!"}
+      </h2>
+
+      <Pie right={Number(searchParams.get("correct"))} wrong={wrong} />
+      <h2 className="my-4 text-xl font-bold">Your Test Result</h2>
+      <p>{id}</p>
+      <p className="text-red-500">
+        {/* {Math.round(
+          (Number(searchParams.correct) / Number(searchParams.total)) * 100,
+        ) + "%"} */}
+        {Number(searchParams.get("result")) + "%"}
+      </p>
+      <Button asChild className="mt-4">
+        <Link
+          href={{
+            pathname: `/exam/${searchParams.get("id")}/report`,
+            query: {
+              id: `${searchParams.get("id")}`,
+              choiceId: `${searchParams.get("choiceId")}`,
+            },
+          }}
+        >
+          get details report
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default FrontResult;
