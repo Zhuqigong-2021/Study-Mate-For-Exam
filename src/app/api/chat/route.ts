aea13e702cfce1d669ca57163dc860aa846b8001
Zhuqigong-2021 <1406846879@qq.com>
2024-02-08 00:35:51 -0500

    fix the max token issue with right model now

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -41,21 +41,61 @@ export async function POST(req: Request) {
     console.log("Relevant notes found: ", JSON.stringify(relevantNotes));
     const systemMessage: ChatCompletionMessage = {
       role: "assistant",
+      // content: `Note Summaries:\n${relevantNotes
+      //   ?.map((note) => {
+      //     const desc =
+      //       note.description.length > 50
+      //         ? `${note.description.substring(0, 50)}...`
+      //         : note.description; // Concisely summarize description
+
+      //     const questionsDetails = note.questions
+      //       .map((q, idx) => {
+      //         const questionSummary =
+      //           q.questionTitle.length > 30
+      //             ? `${q.questionTitle.substring(0, 30)}...`
+      //             : q.questionTitle; // Summarize question text
+      //         const choicesSummary = q.choices
+      //           .map((choice, cIdx) => `${cIdx + 1}: ${choice}`)
+      //           .join(", "); // List choices
+      //         const correctAnswer = `Correct: ${q.choices.find(
+      //           (c) => c.answer == true,
+      //         )}`;
+      //         return `Q${
+      //           idx + 1
+      //         }: ${questionSummary} Choices: [${JSON.stringify(
+      //           choicesSummary,
+      //         )}] ${JSON.stringify(correctAnswer)}`;
+      //       })
+      //       .join("; ");
+
+      //     return `Title: ${note.title}, Description: ${desc}, Questions: ${questionsDetails}`;
+      //   })
+      //   .join("\n\n")}`,
       content:
         "You are an intelligent note-taking app. You answer the user's question based on their existing notes." +
         "The relevant notes for this query are: \n" +
         relevantNotes
           ?.map(
-            (note) =>
-              `Title: ${note.title}\n\nDescription:\n${
+            (note) => {
+              const questions = note.questions.map((q) => {
+                const choices = q.choices.map(
+                  (choice) => choice.content + choice.answer,
+                );
+                return q.questionTitle + JSON.stringify(choices);
+              });
+
+              return `Title: ${note.title}\nDescription:\n${
                 note.description
-              }\n\nQuestions:${JSON.stringify(note.questions)}`,
+              }\nQuestions:${JSON.stringify(questions)}`;
+            },
+
+            // \n\nQuestions:${ JSON.stringify(note.questions) }`,
           )
           .join("\n\n"),
     };
-
+    console.log(systemMessage);
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-1106",
       stream: true,
       messages: [systemMessage, ...messagesTruncated],
     });
