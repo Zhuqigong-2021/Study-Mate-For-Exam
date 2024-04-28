77eeaeef90833d65bd050224a74ba8e457c64d19
Zhuqigong-2021 <1406846879@qq.com>
2024-04-28 19:11:50 -0400

    try with empty socket ()

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000" // Development URL
-  : "https://study-mate-for-exam.vercel.app";
+  : "https://study-mate-for-exam.vercel.app/";
--- src/socket.ts
+++ src/socket.ts
@@ -4,4 +4,4 @@ import { io } from "socket.io-client";
 // const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000";
 // const socketUrl = "http://localhost:3000";
 import { server } from "../config";
-export const socket = io(server);
+export const socket = io();
