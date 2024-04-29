f131846bd466b02b81581dcd38e52e07e49c42a0
Zhuqigong-2021 <1406846879@qq.com>
2024-04-29 00:13:38 -0400

    remove default real variable and add it in vercel directly

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -5,9 +5,9 @@ import PusherClient from "pusher-js";
 // PUSHER_SECERT=2f4d1750a7b4116bb198
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID! || "1795051",
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY! || "26f260f45e9d0d47f175",
-  secret: process.env.PUSHER_SECERT! || "2f4d1750a7b4116bb198",
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  secret: process.env.PUSHER_SECERT!,
   cluster: "us3",
   useTLS: true,
 });
